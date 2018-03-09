c0dd7ea57bf07e0a6d39ffc1da1cf616b8db701f
Kevin B <biojoutk1106@gmail.com>
2018-03-09 18:01:55 +0100

    Let's hope it'll be ok

--- src/Web/routes/admin.js
+++ src/Web/routes/admin.js
@@ -1,24 +1,16 @@
 const { Router } = require('express');
 const client = require('../../index');
 
+const articles = require('./admin/articles');
+
 const router = Router()
   .use((req, res, next) => {
     if (req.isAuthenticated() && client.config.owners.includes(req.user.id)) return next();
     else return res.render('error.pug', { errorCode: '403' });
   })
+  .use('/articles', articles)
   .get('/', (req, res) => {
     res.render('admin.pug');
-  })
-  .post('/sendArticle', async (req, res) => {
-    client.database.insertDocument('articles', {
-      title: req.body.title,
-      content: req.body.content,
-      published: Date.now(),
-      lastEdit: null,
-      author: req.user.id,
-    });
-
-    res.redirect('/admin');
   });
 
 module.exports = router;
---
+++ src/Web/routes/admin/articles.js
@@ -0,0 +1,34 @@
+const { Router } = require('express');
+const client = require('../../../index');
+
+const router = Router()
+  .use((req, res, next) => {
+    if (req.isAuthenticated() && client.config.owners.includes(req.user.id)) return next();
+    else return res.render('error.pug', { errorCode: '403' });
+  })
+  .get('/', (req, res) => {
+    const articles = await client.database.getDocuments('articles');
+    res.render('admin_articles.pug', {
+      articles,
+    });
+  })
+  .post('/post', async (req, res) => {
+    await client.database.insertDocument('articles', {
+      title: req.body.title,
+      content: req.body.content,
+      published: Date.now(),
+      lastEdit: null,
+      author: req.user.id,
+    });
+
+    res.redirect('/admin?success=article');
+  })
+  .get('/delete', async (req, res) => {
+    if (!req.query.id) return res.render('error.pug', { errorCode: '500' });
+
+    client.database.deleteDocument('articles', req.query.id)
+      .then(() => res.redirect('/admin?success=article'))
+      .catch(() => res.render('error.pug', { errorCode: '500' }));
+  });
+
+module.exports = router;
