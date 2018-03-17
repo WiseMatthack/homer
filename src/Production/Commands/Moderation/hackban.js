b12e9d87f4ab41ab9b8be87e3e7904722ad744b9
iDroid <biojoutk1106@gmail.com>
2018-03-17 19:56:39 +0100

    Kick and hackban commands (derived from ban)

---
+++ src/Production/Commands/Moderation/hackban.js
@@ -0,0 +1,48 @@
+const Command = require('../../../Core/Structures/Command');
+
+class Hackban extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'hackban',
+      botPermissions: ['BAN_MEMBERS'],
+      userPermissions: ['BAN_MEMBERS'],
+      category: 5,
+    });
+  }
+
+  async run(ctx) {
+    const search = ctx.args[0];
+    const reason = ctx.args.slice(1).join(' ') || ctx.__('moderation.common.noReason');
+
+    if (!search) return ctx.channel.send(ctx.__('hackban.error.noSearch', {
+      errorIcon: this.client.constants.statusEmotes.error,
+    }));
+
+    const targetUser = (await this.client.fetchUser(search)) || null;
+    if (!targetUser) return ctx.channel.send(ctx.__('hackban.error.noUser', {
+      errorIcon: this.client.constants.statusEmotes.error,
+    }));
+
+    if (ctx.guild.members.has(targetUser.id)) return ctx.channel.send(ctx.__('hackban.error.forbidden', {
+      errorIcon: this.client.constants.statusEmotes.error,
+      tag: targetUser.tag,
+    }));
+
+    ctx.guild.ban(targetUser.id, {
+      days: 7,
+      reason: `${ctx.author.tag}: ${reason}`,
+    })
+      .then(() => {
+        this.client.moderation.registerCase(ctx.guild.id, 3, ctx.author.id, targetUser.id, reason);
+        ctx.channel.send(ctx.__('hackban.success', {
+          successIcon: this.client.constants.statusEmotes.success,
+          tag: targetMember.user.tag,
+        }));
+      })
+      .catch(error => ctx.channel.send(ctx.__('hackban.error', {
+        error,
+      })));
+  }
+}
+
+module.exports = Hackban;
---
+++ src/Production/Commands/Moderation/kick.js
@@ -0,0 +1,45 @@
+const Command = require('../../../Core/Structures/Command');
+
+class Kick extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'kick',
+      botPermissions: ['KICK_MEMBERS'],
+      userPermissions: ['KICK_MEMBERS'],
+      category: 5,
+    });
+  }
+
+  async run(ctx) {
+    const search = ctx.args[0];
+    const reason = ctx.args.slice(1).join(' ') || ctx.__('moderation.common.noReason');
+
+    if (!search) return ctx.channel.send(ctx.__('kick.error.noSearch', {
+      errorIcon: this.client.constants.statusEmotes.error,
+    }));
+
+    const targetMember = ctx.mentions.members.first() || ctx.guild.members.get(search) || null;
+    if (!targetMember) return ctx.channel.send(ctx.__('kick.error.noMember', {
+      errorIcon: this.client.constants.statusEmotes.error,
+    }));
+
+    if (!this.client.moderation.canInteract(ctx.member, targetMember, ctx.guild.me)) return ctx.channel.send(ctx.__('moderation.common.cannotInteract', {
+      errorIcon: this.client.constants.statusEmotes.error,
+      tag: targetMember.user.tag,
+    }));
+
+    targetMember.kick(`${ctx.author.tag}: ${reason}`)
+      .then(() => {
+        this.client.moderation.registerCase(ctx.guild.id, 2, ctx.author.id, targetMember.id, reason);
+        ctx.channel.send(ctx.__('kick.success', {
+          successIcon: this.client.constants.statusEmotes.success,
+          tag: targetMember.user.tag,
+        }));
+      })
+      .catch(error => ctx.channel.send(ctx.__('kick.error', {
+        error,
+      })));
+  }
+}
+
+module.exports = Kick;
