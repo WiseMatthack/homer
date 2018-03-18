cba3d943150fb2dcaabf9deb6dd9ada577055f8e
iDroid <biojoutk1106@gmail.com>
2018-03-18 08:14:50 +0100

    Typo xD

--- src/Production/Commands/General/google.js
+++ src/Production/Commands/General/google.js
@@ -19,7 +19,7 @@ class Google extends Command {
 
     const query = encodeURIComponent(search);
     snekfetch
-      .get(`https://www.googleapis.com/customsearch/v1?key=${this.client.config.api.googleKey}&cx=${this.client.config.api.googleCx}&lr=lang_${ctx.settings.data.misc.locale.split('-')[0]}&num=1&filter=1&safe=${ctw.channel.nsfw ? 'off' : 'high'}&fields=queries(request(totalResults)),items(link)&q=${query}`)
+      .get(`https://www.googleapis.com/customsearch/v1?key=${this.client.config.api.googleKey}&cx=${this.client.config.api.googleCx}&lr=lang_${ctx.settings.data.misc.locale.split('-')[0]}&num=1&filter=1&safe=${ctx.channel.nsfw ? 'off' : 'high'}&fields=queries(request(totalResults)),items(link)&q=${query}`)
       .then((response) => {
         const parsed = response.body;
 
