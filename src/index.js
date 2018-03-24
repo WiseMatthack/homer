a6f68dbca2263e1d0ebc06194f9b7e34f26ec136
iDroid <biojoutk1106@gmail.com>
2018-03-24 14:28:35 +0100

    If it hadn't been for cotton-eye Joe 🎵

--- src/index.js
+++ src/index.js
@@ -35,6 +35,18 @@ module.exports = client;
 process.on('unhandledRejection', (error) => {
   if (error instanceof DiscordAPIError) return;
   console.error(error);
+
+  for (const ownerID of client.config.owners) {
+    const user = client.users.get(ownerID);
+    user.send(`If it hadn't been for cotton-eye Joe\nI'd been married long time ago\nWhere did you come from. Where did you go ?\nWhere did you come from cotton-eye Joe ?\n\n\`\`\`js\n${error.message}\`\`\``, {
+      video: {
+        url: 'https://www.youtube.com/watch?v=mOYZaiDZ7BM',
+        height: 100,
+        width: 100,
+      },
+    })
+      .catch(() => {}); // No but shit I won't handle the error of an error
+  }
 });
 
 process.on('uncaughtException', async (err) => {
