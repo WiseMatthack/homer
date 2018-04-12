fdf33c4bc38819a07eb6e6e86cb6be9c3e4d6e34
iDroid <biojoutk1106@gmail.com>
2018-04-12 20:30:50 +0200

    Of course

--- src/Production/Events/UserUpdate.js
+++ src/Production/Events/UserUpdate.js
@@ -6,13 +6,15 @@ class UserUpdate extends Event {
   }
 
   async handle(oldUser, newUser) {
-    const nameObject = (await this.client.database.getDocument('names', oldUser.id)) || ({
-      id: oldUser.id,
-      names: [],
-    });
+    if (oldUser.username !== newUser.username) {
+      const nameObject = (await this.client.database.getDocument('names', oldUser.id)) || ({
+        id: oldUser.id,
+        names: [],
+      });
 
-    nameObject.names.push(oldUser.username);
-    this.client.database.insertDocument('names', nameObject, { conflict: 'update' });
+      nameObject.names.push(oldUser.username);
+      this.client.database.insertDocument('names', nameObject, { conflict: 'update' });
+    }
   }
 }
 
