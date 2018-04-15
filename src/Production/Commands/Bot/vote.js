8c46c360313108fa4ebeafc1cc3fd95c40082b28
iDroid <biojoutk1106@gmail.com>
2018-04-15 21:33:55 +0200

    Votes command is back

---
+++ src/Production/Commands/Bot/vote.js
@@ -0,0 +1,45 @@
+const Command = require('../../../Core/Structures/Command');
+const { RichEmbed } = require('discord.js');
+
+class Vote extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'about',
+      category: 1,
+    });
+  }
+
+  async run(ctx) {
+    const list = await this.client.database.getDocuments('votes').then(list => list.sort((a, b) => b.count - a.count));
+    const message = (list.length === 0 ? [ctx.__('vote.noVoter')] : []);
+
+    for (let i = 0; i < list.length; i += 1) {
+      if (i > 5) return;
+
+      const voteEntry = list[i];
+      const userTag = this.client.fetchUser(voteEntry.id).then(u => u.tag);
+
+      let medalEmote = '🏅';
+      if (i === 0) medalEmote = '🥇';
+      else if (i === 1) medalEmote = '🥈';
+      else if (i === 2) medalEmote = '🥉';
+
+      message.push(ctx.__('vote.voteEntry', {
+        tag: userTag,
+        emote: medalEmote,
+        count: voteEntry.count,
+      }));
+    }
+
+    const embed = new RichEmbed()
+      .setDescription(ctx.__('vote.embedDescription', {
+        botID: this.client.user.id,
+        topVoters: message.join('\n'),
+      }))
+      .setColor(ctx.guild.me.displayHexColor);
+
+    ctx.channel.send(ctx.__('vote.title'), { embed });
+  }
+}
+
+module.exports = Vote;
