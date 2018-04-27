3f6df20f47754b0ce0886841d9e92c5060f6578a
iDroid <biojoutk1106@gmail.com>
2018-04-27 11:37:49 +0200

    Guide command

---
+++ src/Production/Commands/Bot/guide.js
@@ -0,0 +1,33 @@
+const Command = require('../../../Core/Structures/Command');
+const possibleReactions = ['☎'];
+
+class Guide extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'guide',
+      category: 1,
+    });
+  }
+
+  async run(ctx) {
+    const sentMessage = await ctx.channel.send(ctx.__('guide.welcome'));
+
+    for (const reaction of possibleReactions) {
+      await sentMessage.react(reaction);
+    }
+
+    sentMessage.awaitReactions(
+      (reaction, user) => user.id === ctx.author.id &&
+        possibleReactions.includes(reaction.emoji.name),
+      { limit: 1 },
+    )
+      .then(async (reactions) => {
+        await sentMessage.clearReactions();
+        sentMessage.edit(ctx.__(`guide.${reactions.first().emoji.name}`, {
+          prefix: this.client.config.discord.defaultPrefixes[0],
+        }));
+      });
+  }
+}
+
+module.exports = Guide;
