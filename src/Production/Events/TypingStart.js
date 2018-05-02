c534662c548e7edd4dcc35922ad10c2ffee2b6f2
iDroid <biojoutk1106@gmail.com>
2018-05-02 19:29:40 +0200

    Fixed typing

--- src/Production/Events/TypingStart.js
+++ src/Production/Events/TypingStart.js
@@ -6,11 +6,17 @@ class TypingStart extends Event {
   }
 
   async handle(channel) {
-    const phoneCall = this.client.phone.calls.find(call => call.sender === channel.id || call.receiver === channel.id);
+    const phoneCall = this.client.phone.calls.find(call =>
+      call.state === 1 &&
+      (call.sender === channel.id || call.receiver === channel.id));
     if (phoneCall) {
       const type = phoneCall.sender === channel.id ? 'receiver' : 'sender';
-      const targetChannel = this.client.channels.get(phoneCall[type]);
-      if (!targetChannel) return;
+      const targetGuild = this.client.guilds.get(phoneCall[type]);
+      if (!targetGuild) return;
+
+      const targetChannel = targetGuild.channels.get(
+        await this.client.database.getDocument('guild', targetGuild.id).then(s => s.phone.channel),
+      );
 
       targetChannel.startTyping();
     }
--- src/Production/Events/TypingStop.js
+++ src/Production/Events/TypingStop.js
@@ -11,8 +11,12 @@ class TypingStop extends Event {
     const phoneCall = this.client.phone.calls.find(call => call.sender === channel.id || call.receiver === channel.id);
     if (phoneCall) {
       const type = phoneCall.sender === channel.id ? 'receiver' : 'sender';
-      const targetChannel = this.client.channels.get(phoneCall[type]);
-      if (!targetChannel) return;
+      const targetGuild = this.client.guilds.get(phoneCall[type]);
+      if (!targetGuild) return;
+
+      const targetChannel = targetGuild.channels.get(
+        await this.client.database.getDocument('guild', targetGuild.id).then(s => s.phone.channel),
+      );
 
       targetChannel.stopTyping();
     }
