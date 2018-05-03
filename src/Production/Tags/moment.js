b6d47ee465d7841295565a0adea25eb0d167eddb
iDroid <biojoutk1106@gmail.com>
2018-05-03 21:30:08 +0200

    Fixed dis

--- src/Production/Tags/moment.js
+++ src/Production/Tags/moment.js
@@ -2,12 +2,12 @@ const TagFunction = require('../../Core/Structures/TagFunction');
 const moment = require('moment-timezone');
 
 class MomentFunction extends TagFunction {
-  run(args, context) {
+  run(args, context, contextType, settings) {
     const format = args[0];
     const timezone = args[1] || 'UTC';
 
     return moment()
-      .locale(context.settings.data.misc.locale)
+      .locale(settings.misc.locale)
       .tz(timezone)
       .format(format);
   }
