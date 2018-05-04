8800f82a02039e6e389a5797bebeec226b07d566
iDroid <biojoutk1106@gmail.com>
2018-05-04 12:35:35 +0200

    WHYYYYY

--- src/Production/Events/TypingStop.js
+++ src/Production/Events/TypingStop.js
@@ -11,13 +11,15 @@ class TypingStop extends Event {
     const phoneCall = this.client.phone.calls.find(call =>
       call.state === 1 &&
       (call.sender === channel.guild.id || call.receiver === channel.guild.id));
+    console.log(phoneCall);
     if (phoneCall) {
       const type = phoneCall.sender === channel.guild.id ? 'receiver' : 'sender';
       const targetGuild = this.client.guilds.get(phoneCall[type]);
-
+      console.log(`Type: ${type} - ID: ${targetGuild.id}`);
       const targetChannel = targetGuild.channels.get(
         await this.client.database.getDocument('guild', targetGuild.id).then(s => s.phone.channel),
       );
+      console.log(`ID: ${targetChannel.id}`)
 
       targetChannel.send('DEBUG TYPINGSTOP EVENT WORKING')
       targetChannel.stopTyping(true);
