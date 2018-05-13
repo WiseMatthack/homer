9e3cfd91f56785876e57ce909cfed77cc0022c32
Kevin B <biojoutk1106@gmail.com>
2018-05-13 20:53:22 +0200

    Good thing

--- src/Production/Commands/configuration/locale.js
+++ src/Production/Commands/configuration/locale.js
@@ -1,11 +1,11 @@
 const Command = require('../../../Core/Structures/Command');
 const i18n = require('i18n');
 
-class Locale extends Command {
+class Language extends Command {
   constructor(client) {
     super(client, {
-      name: 'locale',
-      aliases: ['setlocale', 'language'],
+      name: 'language',
+      aliases: ['locale', 'translations'],
       userPermissions: ['MANAGE_GUILD'],
       category: 'configuration',
     });
@@ -13,28 +13,50 @@ class Locale extends Command {
 
   async run(ctx) {
     const locale = ctx.args[0];
-    if (!locale) return ctx.channel.send(ctx.__('locale.localeSet', {
-      code: ctx.__('lang.code'),
-      fullName: ctx.__('lang.fullName'),
-      emote: ctx.__('lang.flagEmote'),
-    }));
 
-    if (!i18n.getLocales().includes(locale)) return ctx.channel.send(ctx.__('locale.unknownLocale', {
-      errorIcon: this.client.constants.statusEmotes.error,
-      prefix: this.client.config.discord.defaultPrefixes[0],
-      locale,
-    }));
+    if (locale) {
+      if (!i18n.getLocales().includes(locale)) return ctx.channel.send(ctx.__('locale.unknownLocale', {
+        errorIcon: this.client.constants.statusEmotes.error,
+        prefix: this.client.config.discord.defaultPrefixes[0],
+        locale,
+      }));
 
-    ctx.settings.data.misc.locale = locale;
-    ctx.setLocale(locale);
-    await ctx.settings.saveData();
+      ctx.settings.data.misc.locale = locale;
+      ctx.setLocale(locale);
+      await ctx.settings.saveData();
 
-    ctx.channel.send(ctx.__('locale.set', {
-      successIcon: this.client.constants.statusEmotes.success,
-      code: ctx.__('lang.code'),
-      fullName: ctx.__('lang.fullName'),
-    }));
+      ctx.channel.send(ctx.__('locale.set', {
+        successIcon: this.client.constants.statusEmotes.success,
+        code: ctx.__('lang.code'),
+        fullName: ctx.__('lang.fullName'),
+      }));
+    } else {
+      const locales = i18n.getLocales();
+
+      const message = [];
+      for (const locale of i18n.getLocales()) {
+        const catalog = i18n.getCatalog(locale);
+        message.push(ctx.__('locale.entry', {
+          fullName: catalog['lang.fullName'],
+          emote: catalog['lang.flagEmote'],
+          code: catalog['lang.code'],
+        }));
+      }
+
+      const embed = new RichEmbed()
+        .setDescription(message.join('\n'))
+        .setFooter(ctx.__('locale.footer', {
+          prefix: this.client.config.discord.defaultPrefixes[0],
+        }))
+        .setColor(ctx.guild.me.displayHexColor);
+
+      ctx.channel.send(ctx.__('locale.localeSet', {
+        code: ctx.__('lang.code'),
+        fullName: ctx.__('lang.fullName'),
+        emote: ctx.__('lang.flagEmote'),
+      }), { embed });
+    }
   }
 }
 
-module.exports = Locale;
+module.exports = Language;
