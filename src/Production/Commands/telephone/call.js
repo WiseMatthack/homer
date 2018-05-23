08427b420adc398093c2a61e5e38fdf354354f9d
Kevin B <biojoutk1106@gmail.com>
2018-05-23 18:29:20 +0200

    Let's not let random call the support line

--- src/Production/Commands/telephone/call.js
+++ src/Production/Commands/telephone/call.js
@@ -34,7 +34,7 @@ class Call extends Command {
 
     const settingsToCall = ctx.args[0] === 'random' ?
       (await this.client.database.getDocuments('guild').then((servers) => {
-        const filteredServers = servers.filter(s => s.phone.number && s.phone.phonebook && this.client.channels.has(s.phone.channel));
+        const filteredServers = servers.filter(s => (s.phone.number && s.phone.phonebook && this.client.channels.has(s.phone.channel)) && s.phone.number !== '1-SUPPORT');
         return filteredServers[Math.round(Math.random() * filteredServers.length)];
       })) : (await this.client.database.provider.table('guild').filter({ phone: { number } }).then(s => s[0] || null));
 
