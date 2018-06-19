2b120268a80702559cda04ca46a9d340976f4098
Kevin B <biojoutk1106@gmail.com>
2018-06-19 20:54:20 +0200

    Fixed angle thing

--- src/commands/general/weather.js
+++ src/commands/general/weather.js
@@ -102,7 +102,7 @@ class WeatherCommand extends Command {
 
   getDirection(angle) {
     const arrayIndex = Number((angle / 22.5) + 0.5);
-    return Math.round(arrayIndex % 16) - 1;
+    return Math.round(arrayIndex % 15);
   }
 
   get franceDepartments() {
