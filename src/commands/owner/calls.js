c508a0cb03a9792b08d0d841d282edff02525e88
Kevin B <biojoutk1106@gmail.com>
2018-06-20 21:00:28 +0200

    Calls (private) command

---
+++ src/commands/owner/calls.js
@@ -0,0 +1,26 @@
+const Command = require('../../structures/Command');
+const Menu = require('../../structures/Menu');
+
+class CallsCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'calls',
+      private: true,
+      dm: true,
+    });
+  }
+
+  async execute(context) {
+    const calls = await this.client.database.getDocuments('calls');
+    if (calls.length === 0) return context.replyWarning('There is no ongoing call at the moment.');
+
+    const menu = new Menu(
+      context,
+      calls.map(call => `${this.dot} Sender: **${call.sender.number}** - Receiver: **${call.receiver.number}** - Created ${this.client.time.timeSince(call.time, 'en-gb', false, true)}`),
+    );
+
+    menu.send('📞 Ongoing telephone calls:');
+  }
+}
+
+module.exports = CallsCommand;
