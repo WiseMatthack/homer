41a5a99374e7dd2d776ab3bc7deb5d6f9325ffbe
Kevin B <biojoutk1106@gmail.com>
2018-06-24 11:57:01 +0200

    Improved translation a bit

---
+++ src/commands/owner/reboot.js
@@ -0,0 +1,18 @@
+const Command = require('../../structures/Command');
+
+class RebootCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'reboot',
+      category: 'owner',
+      private: true,
+      dm: true,
+    });
+  }
+
+  async execute(context) {
+    // Todo
+  }
+}
+
+module.exports = RebootCommand;
