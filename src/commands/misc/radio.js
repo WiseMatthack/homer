e6c71df0a5541b98406d7ea096271aacedfc58bc
Kevin B <biojoutk1106@gmail.com>
2018-07-07 13:33:41 +0200

    First test

--- src/commands/misc/radio.js
+++ src/commands/misc/radio.js
@@ -6,7 +6,13 @@ class RadioCommand extends Command {
     super(client, {
       name: 'radio',
       category: 'misc',
-      children: [new ListSubcommand(client)],
+      children: [
+        new ListSubcommand(client),
+        new TuneSubcommand(client),
+        new VolumeSubcommand(client),
+        new StopSubcommand(client),
+        new ChannelSubcommand(client),
+      ],
     });
   }
 
@@ -33,6 +39,123 @@ class ListSubcommand extends Command {
     );
 
     menu.send(context.__('radio.list.title', { name: this.client.user.username }));
+  }
+}
+
+class TuneSubcommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'tune',
+      category: 'misc',
+      usage: '<frequency>',
+      private: true,
+    });
+  }
+
+  async execute(context) {
+    const channel = context.message.guild.channels.get(context.settings.radio.channel);
+    if (!channel) return context.replyWarning(context.__('radio.noRadioChannel', { prefix: this.client.prefix }));
+    if (!channel.joinable || !channel.speakable) return context.replyError(context.__('radio.cannotJoinOrSpeak', { name: channel.name }));
+    if (!channel.members.has(context.message.author.id)) return context.replyWarning(context.__('radio.notInChannel'));
+
+    const frequency = context.args[0];
+    if (!frequency) return context.replyError(context.__('radio.tune.noFrequency'));
+
+    const radio = await this.client.database.getDocument('radios', frequency);
+    if (!radio) return context.replyWarning(context.__('radio.tune.noRadioFound', { frequency }));
+
+    let connection = this.client.voiceConnections.get(context.message.guild.id);
+    if (!connection) await channel.join();
+
+    const hq = (this.client.config.owners.includes(context.message.author.id) || await this.client.database.getDocument('donators', context.message.author.id));
+    const message = await context.message.channel.send(context.__('radio.tune.tunning', { name: radio.name }));
+    const dispatcher = await connection.playStream(
+      radio.url,
+      {
+        volume: context.settings.radio.volume || 0.5,
+        bitrate: hq ? 96 : 48,
+      },
+    );
+
+    dispatcher.once('speaking', () => message.edit(context.__('radio.tune.playing', { name: radio.name })));
+  }
+}
+
+class VolumeSubcommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'volume',
+      category: 'misc',
+      usage: '<volume (0-100)>',
+      private: true,
+    });
+  }
+
+  async execute(context) {
+    let volume = context.args[0];
+    if (isNaN(parseInt(volume)) || volume < 0 || volume > 100) return context.replyError(context.__('radio.volume.invalidVolume'));
+
+    volume = (volume / 100).toFixed(1);
+    await this.client.database.updateDocument('settings', context.message.guild.id, {
+      radio: { volume },
+    });
+
+    const currentBroadcast = this.client.voiceConnections.get(context.message.guild.id);
+    if (currentBroadcast && currentBroadcast.dispatcher) await currentBroadcast.dispatcher.setVolume(volume);
+
+    context.replySuccess(context.__('radio.volume.set', { volume: (volume * 100) }));
+  }
+}
+
+class StopSubcommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'stop',
+      category: 'misc',
+      private: true,
+    });
+  }
+
+  async execute(context) {
+    const channel = context.message.guild.channels.get(context.settings.radio.channel);
+    if (!channel) return context.replyWarning(context.__('radio.noRadioChannel', { prefix: this.client.prefix }));
+    if (!channel.joinable || !channel.speakable) return context.replyError(context.__('radio.cannotJoinOrSpeak', { name: channel.name }));
+    if (!channel.members.has(context.message.author.id)) return context.replyWarning(context.__('radio.notInChannel'));
+
+    let connection = this.client.voiceConnections.get(context.message.guild.id);
+    if (!connection) return context.replyWarning(context.__('radio.stop.noActiveStream', { name: connection.channel.name }));
+    await channel.leave();
+
+    context.replySuccess(context.__('radio.stop.done'));
+  }
+}
+
+class ChannelSubcommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'channel',
+      category: 'misc',
+      usage: '[channel]',
+      userPermissions: ['MANAGE_GUILD'],
+      private: true,
+    });
+  }
+
+  async execute(context) {
+    const search = context.args.join(' ');
+    let channel = context.message.guild.channels.filter(c => c.type === 'voice').find(c => c.members.has(context.message.author.id));
+    if (search) {
+      const foundChannels = this.client.finder.findRolesOrChannels(context.message.guild.channels.filter(c => c.type === 'voice'), search);
+      if (!foundChannels || foundChannels.length === 0 || !foundChannels[0]) return context.replyError(context.__('finderUtil.findChannels.zeroResult', { search }));
+      else if (foundChannels.length === 1) channel = foundChannels[0];
+      else if (foundChannels.length > 1) return context.replyWarning(this.client.finder.formatChannels(foundChannels, context.settings.misc.locale));
+    }
+    if (!channel) return context.replyWarning(context.__('radio.channel.noChannelFound'));
+
+    context.settings.radio.channel = channel.id;
+    await context.saveSettings();
+
+    context.replySuccess(context.__('radio.channel.set', { name: channel.name }));
   }
 }
 
